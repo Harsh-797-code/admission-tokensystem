4bfc191e1f2847b46b4358644a415a6f40786648
harshitha797-code <harshithammanohar07092007@gmail.com>
2026-08-13 13:03:04 +0530

    port changed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7777;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
